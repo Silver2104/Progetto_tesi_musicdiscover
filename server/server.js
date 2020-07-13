b20efa5ed1c2a26a1dc4d732730df3728ee606b8
Silver2104 <stefano.rughetti@gmail.com>
2020-07-13 19:40:19 +0200

    Update server.js
    aggiunta commenti

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-var express = require("express");
-var request = require("request");
-var app = express();
+var express = require("express"); // Web Framework
+var request = require("request"); // Web Scraper
+var app = express(); // Inizializzazione express
 
+// Routing principale
 app.get("/", function (req, res) {
   res.send("Hello World!");
+  //Chiamata alla funzione get_page che prende l'html di una pagina
   const pagina_html = get_page();
   console.log(pagina_html);
 });
 
+// Avvio server sulla porta 3000
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
 });
 
+//Funzione per prendere l'html di una pagina
 function get_page() {
   request("http://www.google.com", function (error, response, body) {
     console.error("error:", error); // Print the error if one occurred
