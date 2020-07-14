2bddcbe9ae5e7a2b0ab3ebf6469ba935ff9defdf
Silver2104 <stefano.rughetti@gmail.com>
2020-07-14 20:12:14 +0200

    Update server.js
    Aggiunta funzione per prendere il link di download dalla pagina di una canzone su bootcamp

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
+//IMPORT PACKAGES
 var express = require("express"); // Web Framework
 var request = require("request"); // Web Scraper
-var app = express(); // Inizializzazione express
+//FINE IMPORT PACKAGES
+
+//INIZIALIZZAZIONE PACKAGES
+var app = express();
+//FINE INIZIALIZZAZIONE PACKAGES
+
+// Costanti di debug ------------- PRENDERE QUESTE VARIABILI FRAMITE FORM NEL FRONT END --------------
+const link_html = "https://noveller.bandcamp.com/track/rune";
 
 // Routing principale
 app.get("/", function (req, res) {
   res.send("Hello World!");
-  //Chiamata alla funzione get_page che prende l'html di una pagina
-  const pagina_html = get_page();
-  console.log(pagina_html);
+  //--------------- SEPARARE IL ROUTING DALLA CASCATA DI FUNZIONI ? --------------
+  // Inizio promessa per prendere pagina html
+  get_page(link_html)
+    //Una volta preso il testo della pagina cerco con le espressioni regolari i link di download
+    .then((testo_pagina) => {
+      var rePattern = new RegExp(/"mp3\-128":"([^"]*)/);
+      var arrMatches = testo_pagina.match(rePattern);
+      console.log(arrMatches[1]);
+    });
+  //--------------- SEPARARE IL ROUTING DALLA CASCATA DI FUNZIONI ? --------------
 });
 
 // Avvio server sulla porta 3000
@@ -16,10 +31,16 @@ app.listen(3000, function () {
 });
 
 //Funzione per prendere l'html di una pagina
-function get_page() {
-  request("http://www.google.com", function (error, response, body) {
-    console.error("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    console.log("body:", body); // Print the HTML for the Google homepage.
+const get_page = (link) => {
+  return new Promise((resolve, reject) => {
+    const testo_pagina = request(link, function (error, response, body) {
+      console.error("error:", error); // Print the error if one occurred
+      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      if (body != "") {
+        resolve(body);
+      } else {
+        reject(Error("Nessun testo ottenuto dal link"));
+      }
+    });
   });
-}
+};
