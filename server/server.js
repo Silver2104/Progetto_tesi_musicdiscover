bb69bd0fe0a28f2e6e298e18c02a645c2f1ad0e0
Silver2104 <stefano.rughetti@gmail.com>
2020-07-15 16:51:39 +0200

    Rimozione request, aggiunta got
    Rimozione del package request perché non più in mantenimento e al suo posto ho aggiunto got per gestire il paradigma REST.

--- server/server.js
+++ server/server.js
@@ -1,40 +1,34 @@
 //IMPORT PACKAGES
-var express = require("express"); // Web Framework
-var request = require("request"); // Web Scraper
-var fs = require("fs"); // File System library
+const stream = require("stream"); //Per creare stream di download
+const { promisify } = require("util"); // Per fare in modo che la pipeline sia eseguita in modo lineare e non parallelo ad altro
+const express = require("express"); // Web Framework
+const got = require("got"); // Per utilizzare il paradigma REST
+const fs = require("fs"); // File System library
+
 //FINE IMPORT PACKAGES
 
 //INIZIALIZZAZIONE PACKAGES
 var app = express();
 //FINE INIZIALIZZAZIONE PACKAGES
 
-// Costanti di debug ------------- PRENDERE QUESTE VARIABILI FRAMITE FORM NEL FRONT END --------------
+//Costanti di debug ------------- PRENDERE QUESTE VARIABILI FRAMITE FORM NEL FRONT END --------------
 const link_html = "https://noveller.bandcamp.com/track/rune";
 var dest = "canzone.mp3";
-var file = fs.createWriteStream(dest);
+//Fine Costanti di debug
+
+//Costanti
+
+//FINE Costanti
 
 // Routing principale
 app.get("/", function (req, res) {
   res.send("Hello World!");
-  //--------------- SEPARARE IL ROUTING DALLA CASCATA DI FUNZIONI ? --------------
   // Inizio promessa per prendere pagina html
   get_page(link_html)
     //Una volta preso il testo della pagina cerco con le espressioni regolari i link di download
     .then((testo_pagina) => {
-      //Creo il pattern per trovare il link di download della canzone ---------------- VEDERE SE DA ANCHE LINK IN CASO DI ALBUM -----------------
-      var rePattern = new RegExp(/"mp3\-128":"([^"]*)/);
-      //Creo la lista con i risultati
-      var arrMatches = testo_pagina.match(rePattern);
-      //Scarico la canzone
-      request
-        .get(arrMatches[1])
-        .on("error", function (err) {
-          console.log(err);
-        })
-        .pipe(fs.createWriteStream(dest));
-      console.log(arrMatches[1]);
+      download_canzone(testo_pagina, dest);
     });
-  //--------------- SEPARARE IL ROUTING DALLA CASCATA DI FUNZIONI ? --------------
 });
 
 // Avvio server sulla porta 3000
@@ -45,14 +39,34 @@ app.listen(3000, function () {
 //Funzione per prendere l'html di una pagina
 const get_page = (link) => {
   return new Promise((resolve, reject) => {
-    const testo_pagina = request(link, function (error, response, body) {
-      console.error("error:", error); // Print the error if one occurred
-      console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-      if (body != "") {
-        resolve(body);
-      } else {
-        reject(Error("Nessun testo ottenuto dal link"));
+    (async () => {
+      try {
+        const response = await got(link);
+        if (response.body != null) {
+          resolve(response.body);
+        } else {
+          reject(Error("Nessun testo ottenuto dal link"));
+        }
+        //=> '<!doctype html> ...'
+      } catch (error) {
+        console.log(error.response.body);
+        //=> 'Internal server error ...'
       }
-    });
+    })();
   });
+};
+
+//Funzione download canzone da un link
+const download_canzone = (source, dest) => {
+  //Creo la pipeline su cui passeranno i bytes
+  const pipeline = promisify(stream.pipeline);
+  //Creo il pattern per trovare il link di download della canzone ---------------- VEDERE SE DA ANCHE LINK IN CASO DI ALBUM -----------------
+  var rePattern = new RegExp(/"mp3\-128":"([^"]*)/);
+  //Creo la lista con i risultati
+  var arrMatches = source.match(rePattern);
+  //Scarico la canzone
+  (async () => {
+    console.log(arrMatches[1]); //------------- LOG DI CONTROLLO ELIMINARE -----------------
+    await pipeline(got.stream(arrMatches[1]), fs.createWriteStream(dest));
+  })();
 };
