147a2c5bf1fefd3e3a5ea79e9bf387a485c18a4c
Silver2104 <stefano.rughetti@gmail.com>
2020-07-17 12:00:13 +0200

    Basic front end
    Sviluppo di un front end basico

---
+++ musicdiscover_frontend/src/components/download-form.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <p>Sei nella pagina di download delle canzoni</p>
+      </div>
+    );
+  }
+}
