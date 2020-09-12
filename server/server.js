f5d3034a41019888fa3472dc842a730cfdab76cb
Silver2104 <stefano.rughetti@gmail.com>
2020-09-12 16:28:26 +0200

    modifica porta server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,21 @@ app.use("/download", downloadPage);
 app.use("/lastfm", LastfmPage);
 app.use("/twitter", TwitterPage);
 //Fine routes secondari
+function normalizePort(val) {
+  var port = parseInt(val, 10);
 
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 // Avvio server sulla porta 3001
 const port = normalizePort(process.env.PORT || '3000');
 app.listen(port, function () {
